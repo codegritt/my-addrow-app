ac039cfeb161d24cd401da27cf04f1bf22473e34
Gokul <gokulchandan18@gmail.com>
2021-12-23 09:04:54 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +10,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
 
   userForm: FormGroup
+  title: any;
 
   constructor(private fb:FormBuilder){
 this.userForm=this.fb.group({
